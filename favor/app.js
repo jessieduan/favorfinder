946619513d43029f855a1023819d15fbd190943d
Yongxing Deng <yongxingd@gmail.com>
2014-02-03 14:56:55 -0800

    changing port number to 1234

--- favor/app.js
+++ favor/app.js
@@ -12,7 +12,8 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+// 3000 doesn't work for me, so I'm changing it to 1234
+app.set('port', process.env.PORT || 1234);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
