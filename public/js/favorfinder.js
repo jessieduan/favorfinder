c2d09f0d99b9ad3345116e2480fe74558dd3b55c
Ben McKenzie <bpmck@stanford.edu>
2014-02-09 17:21:47 -0800

    More dope styling, need toggle in JS requests

--- public/js/favorfinder.js
+++
@@ -1,9 +0,0 @@
-
-function showFlyout() {
-	if (window.getComputedStyle(document.getElementById("navbar-flyout")).display == 'none'){
-		document.getElementById("navbar-flyout").style.display = 'inline-block';
-	}
-	else {
-		document.getElementById("navbar-flyout").style.display = 'none';
-	}	
-}
---
+++ public/js/requests.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	$('.btn-group').button();
+	$('#inbox').button('toggle');
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+
+	// $('a.friend-link').click(function(e) {
+	// 	e.preventDefault();
+	// });
+}
